502e97a2f542dda91a4940856965f5404f84102d
Nitish Yadav <yadavnitish00028@gmail.com>
2025-07-07 23:57:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import RandomColor from './components/RandomColor/index.jsx'
+
+function App() {
+
+  return (
+    <>
+    <RandomColor/>
+    <div>howeorjwiendfienri</div>
+    </>
+    
+
+  )
+}
+
+export default App
---
+++ src/components/RandomColor/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RandomColor() {
+  return (
+    <div>randomColor</div>
+  )
+}
+
+export default RandomColor
