ba7ac35c09e70724a10b4d9614b34575de540493
Nitish Yadav <yadavnitish00028@gmail.com>
2025-07-08 15:39:22 +0530

    RandomColor component done

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <>
     <RandomColor/>
-    <div>howeorjwiendfienri</div>
     </>
     
 
--- src/components/RandomColor/index.jsx
+++ src/components/RandomColor/index.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function RandomColor() {
+  
+  const [color, setColor] = useState("#ffffff");
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+
+  const handleHexColor = ()=>{
+    const hex="12345678abcdef";
+    let hexColor =`#`;
+    for(let i=0;i<6;i++){
+      const random = Math.floor(Math.random()*14);
+      hexColor = hexColor + hex[random];
+    }
+    setColor(hexColor);
+  }
+
+  const handleRGBColor = ()=>{
+    const r= Math.floor(Math.random()*256);
+    const g= Math.floor(Math.random()*256);
+    const b= Math.floor(Math.random()*256);
+    const rgbColor = `rgb(${r},${g},${b})`;
+    setColor(rgbColor);
+  }
+  
   return (
-    <div>randomColor</div>
+    <div>
+      <div className='h-screen w-screen flex flex-col justify-center' style ={{ backgroundColor: color }}>
+        <div className='text-black my-2 text-center text-5xl' style ={{ backgroundColor: color }} >Current Color is {color} </div>
+        <button className='p-2 rounded bg-black text-white mx-150 my-2' onClick={()=>handleHexColor()}>Generate Hex Color</button>
+        <button className='p-2 rounded bg-black text-white mx-150 my-2' onClick={()=>handleRGBColor()}>Generate RGB Color</button>
+      </div>
+    </div>
   )
 }
 
