a1832243e339efcac02afc01bb173ecdb535fcfd
Nitish Yadav <yadavnitish00028@gmail.com>
2025-07-11 00:41:53 +0530

    component almost complete

--- src/components/ImageSlider/index.jsx
+++ src/components/ImageSlider/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 async function fetchImages() {
-  const response = await fetch("https://picsum.photos/v2/list?page=5&limit=15");
+  const response = await fetch("https://picsum.photos/v2/list?page=5&limit=30");
   const data = await response.json();
   const picArray = data.map((picture) => picture.download_url)
   console.log(picArray);
@@ -10,24 +10,25 @@ async function fetchImages() {
 
 function ImageSlider() {
   const [images, setImages] = useState([]);
-  const [imgIdx, setImgIdx] = useState(0);
+  const [imgIdx, setImgIdx] = useState(15);
 
   const handleClick = async () => {
     const urls = await fetchImages();
     setImages(urls);
   };
 
-  const handleRightArrow = ()=>{
-    
-  }
+  const handleLeftArrow = (prev)=>setImgIdx(prev=>prev-1)
+  const handleRightArrow = (prev)=>setImgIdx(prev=>prev+1)
 
   return (
     <div>
       <div className='flex justify-center my-20'>
-        <button onClick={handleClick} className='bg-blue-400 text-white rounded-3xl p-2'>Load Images</button>
+        <button onClick={()=>handleClick()} className='bg-blue-400 text-white rounded-3xl p-2'>Load Images</button>
       </div>
-      <div>
-        <img src={images[imgIdx]} alt="" className="w-100 h-100 object-cover my-20 mx-130 " />
+      <div className='flex justify-center'>
+        <div className="pt-45 mr-5" onClick={()=>handleLeftArrow(imgIdx)}>{"<--"}</div>
+        <div className='bg-blue-200'><img src={images[imgIdx]} alt="" className="w-100 h-100" /></div>
+        <div className='pt-45 ml-5' onClick={()=>handleRightArrow(imgIdx)}>{"-->"}</div>
       </div>
     </div>
   );
