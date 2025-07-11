7eaa4fbea33152f7a1e800a09bb70a19aa46591c
Nitish Yadav <yadavnitish00028@gmail.com>
2025-07-11 09:15:18 +0530

    useEffect added

--- src/components/ImageSlider/index.jsx
+++ src/components/ImageSlider/index.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 async function fetchImages() {
-  const response = await fetch("https://picsum.photos/v2/list?page=5&limit=5");
+  const response = await fetch("https://picsum.photos/v2/list?page=3&limit=10");
   const data = await response.json();
   const picArray = data.map((picture) => picture.download_url)
   console.log(picArray);
@@ -12,22 +12,29 @@ function ImageSlider() {
   const [images, setImages] = useState([]);
   const [imgIdx, setImgIdx] = useState(0);
 
-  const handleClick = async () => {
-    const urls = await fetchImages();
-    setImages(urls);
-  };
+  const loadImages = async () => {
+      const urls = await fetchImages();
+      setImages(urls);
+      setImgIdx(0)
+    };
+
+  useEffect(() => {  
+    loadImages();
+  }, []);
 
   const handleLeftArrow = (prev)=>setImgIdx((prev) => prev === 0 ? images.length - 1 : prev - 1)
   const handleRightArrow = (prev)=>setImgIdx((prev) => prev === images.length - 1 ? 0 : prev + 1)
+
+
   return (
     <div>
       <div className='flex justify-center my-20'>
-        <button onClick={()=>handleClick()} className='bg-blue-400 text-white rounded-3xl p-2'>Load Images</button>
+        <button onClick={loadImages} className='bg-blue-400 text-white rounded-3xl p-2'>Load Images</button>
       </div>
       <div className='flex justify-center'>
-        <div className="pt-45 mr-5" onClick={()=>handleLeftArrow(imgIdx)}>{"<--"}</div>
+        <div><button className="mr-5 mt-45 p-2 rounded-4xl text-white bg-blue-400" onClick={handleLeftArrow}>{"<--"}</button></div>
         <div className='bg-blue-200'><img src={images[imgIdx]} alt="" className="w-100 h-100" /></div>
-        <div className='pt-45 ml-5' onClick={()=>handleRightArrow(imgIdx)}>{"-->"}</div>
+        <div><button className='ml-5 mt-45 p-2 rounded-4xl text-white bg-blue-400' onClick={handleRightArrow}>{"-->"}</button></div>
       </div>
     </div>
   );
