0eceddd0f407e241c8b43fdb319fccce59c25480
Nitish Yadav <yadavnitish00028@gmail.com>
2025-07-15 15:40:29 +0530

    popup modal done

--- src/components/Popup-Modal/Modal.jsx
+++ src/components/Popup-Modal/Modal.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { X, Download } from 'lucide-react';
 
-const Modal = () => {
+const Modal = ({onClose}) => {
+
+    const modalRef = useRef();
+
+    const closeModal = (e)=>{
+        if(modalRef.current ===e.target){
+            onClose();
+        }
+    }
   return (
-    <div className='fixed inset-0 bg-black/30 backdrop-blur-sm flex justify-center items-center'>
+    <div ref={modalRef} onClick={closeModal} className='fixed inset-0 bg-black/30 backdrop-blur-sm flex justify-center items-center'>
         <div className='mt-10 flex flex-col gap-5 text-white'>
-            <button className='place-self-end'><X size={30}/></button>
+            <button onClick={onClose} className='place-self-end'><X size={30}/></button>
             <div className='bg-indigo-600 px-20 py-10 rounded-xl flex flex-col gap-5 items-center mx-4'>
                 <h1 className='text-3xl font-extrabold'>Download Free Ebook</h1>    
                 <p className='text-3xl font-bold max-w-md text-center'>Want to Learn How to Crack Web Devlopment Interviews Like a Pro!</p>
--- src/components/Popup-Modal/index.jsx
+++ src/components/Popup-Modal/index.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Modal from './Modal'
 
 
 
 const PopUpModal = () => {
+
+    const [showModal, setShowModal] = useState(false);
+
   return (
     <div className='h-screen flex flex-col items-center gap-6 bg-black text-white'>
         <h1 className='text-5xl font-bold mt-5'>Popup Modal</h1>
-        <button className='bg-violet-500 px-4 py-2 rounded-lg text-lg'>Get the ebook</button>
-        <Modal/>
+        <button onClick={()=>setShowModal(true)} className='bg-violet-500 px-4 py-2 rounded-lg text-lg'>Get the ebook</button>
+        {showModal && <Modal onClose={()=>setShowModal(false)}/>}
     </div>
   )
 }
