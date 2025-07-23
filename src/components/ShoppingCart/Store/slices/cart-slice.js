8d78e087019526ff4ef03e6e0e6b231089d3c42a
Nitish Yadav <yadavnitish00028@gmail.com>
2025-07-23 23:24:05 +0530

    cart-slice added

---
+++ src/components/ShoppingCart/Store/slices/cart-slice.js
@@ -0,0 +1,15 @@
+
+
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+  }
+});
+
+export default cartSlice.reducer
