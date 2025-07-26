76685e4967b3f4e5a3797512050d60452ed7ed63
Nitish Yadav <yadavnitish00028@gmail.com>
2025-07-27 03:34:57 +0530

    Pagination component done

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -10,16 +10,28 @@ function Pagination({ currentPage, totalPages = 10, onPageChange }) {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen gap-1">
-      <button className="bg-indigo-600 text-white rounded-full px-3 py-1">
+    <div className="flex gap-1">
+      <button
+        className="bg-indigo-600 text-white rounded-full px-3 py-1 disabled:bg-indigo-300"
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
         Prev
       </button>
       {generateNoOfPages().map((pageNo) => (
-        <button className="bg-blue-500 text-white px-3 py-1 rounded-full">
+        <button
+          className="bg-blue-500 text-white px-3 py-1 rounded-full"
+          key={pageNo}
+          onClick={() => onPageChange(pageNo)}
+        >
           {pageNo}
         </button>
       ))}
-      <button className="bg-indigo-600 text-white rounded-full px-3 py-1">
+      <button
+        className="bg-indigo-600 text-white rounded-full px-3 py-1 disabled:bg-indigo-300"
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      >
         Next
       </button>
     </div>
---
+++ src/components/Pagination/test.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Pagination from ".";
+
+function PaginationTest() {
+  const dummyData = Array.from({ length: 100 }, (_, idx) => ({
+    id: idx,
+    name: `product ${idx + 1}`,
+  }));
+
+  const itemsPerPage = 10;
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastPage = currentPage * itemsPerPage;
+  const indexOfFirstPage = indexOfLastPage - itemsPerPage;
+
+  const currentListOfItems = dummyData.slice(indexOfFirstPage, indexOfLastPage);
+
+  const handleOnPageChange = (currentPage) => setCurrentPage(currentPage);
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen gap-1">
+      <h1 className="text-7xl mb-15">Pagination</h1>
+      <ul className="text-2xl mb-15">
+        {currentListOfItems.map((listItem) => (
+          <li key={listItem.id}>{listItem.name}</li>
+        ))}
+      </ul>
+      <div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={Math.ceil(dummyData.length / itemsPerPage)}
+          onPageChange={handleOnPageChange}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default PaginationTest;
